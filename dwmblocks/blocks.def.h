129f7e32cb625a5cdbf51a083e106121cb4953ab
nico <nico.lenczewski@gmx.de>
2024-10-31 17:45:39 +0100

    cleanup and customisations

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					30,		0},
+	{"", "/home/nico/.config/scrips/name.sh",	          0,		1},
+	{"", "/home/nico/.config/scrips/ip.sh",	            300,		2},
+	{"", "/home/nico/.config/scrips/display.sh",	    120,		3},
+	{"", "/home/nico/.config/scrips/mic.sh",	        300,		4},
+	{"", "/home/nico/.config/scrips/volume.sh",	        300,		5},
+	{"", "/home/nico/.config/scrips/battery.sh",	     60,		6},
+	{"", "/home/nico/.config/scrips/dateTime.sh",        60,		7},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
