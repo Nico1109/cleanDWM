806c2f07e57b9d3fbffcfb9f8f2b5aee60265931
nico <nico.lenczewski@gmx.de>
2024-11-07 21:27:38 +0100

    fix status padding

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -750,7 +750,7 @@ drawbar(Monitor *m)
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
-		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
+		tw = TEXTW(stext) - lrpad - 132; /* 2px right padding */
 		while (1) {
 			if ((unsigned int)*ts > LENGTH(colors)) { ts++; continue ; }
 			ctmp = *ts;
