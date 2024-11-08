6099fcfe70027fb6db43846def310c0f49528cfe
nico <nico.lenczewski@gmx.de>
2024-11-08 02:05:42 +0100

    edit dmenucmd theme

--- dwm/config.h
+++ dwm/config.h
@@ -93,7 +93,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_turkey, "-nf", col_white, "-sb", col_dark_orange, "-sf", col_yellow, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
